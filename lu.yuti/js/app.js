55a65574e65c2f46a1c89e1f1d9b67d0deb8518e
YUTI LU <yuti@YUTIdeMacBook-Pro.local>
2021-03-13 21:40:59 -0800

    update my new design

---
+++ lu.yuti/js/app.js
@@ -0,0 +1,49 @@
+// Document Ready
+$(()=>{
+
+	checkUserId();
+
+   $(document)
+
+   // FORM SUBMITS
+   .on("submit","#signin-form",function(e){
+      e.preventDefault();
+      checkSigninForm();
+   })
+
+
+	// ANCHOR CLICKS
+   .on("click",".js-logout",function(e){
+      sessionStorage.removeItem('userId');
+      checkUserId();
+   })
+
+   // DATA ACTIVATE
+   .on("click","[data-activate]",function(e){
+      let target = $(this).data("activate");
+      $(target).addClass("active");
+   })
+   .on("click","[data-deactivate]",function(e){
+      let target = $(this).data("deactivate");
+      $(target).removeClass("active");
+   })
+   .on("click","[data-toggle]",function(e){
+      let target = $(this).data("toggle");
+      $(target).toggleClass("active");
+   })
+
+   .on("click", ".btn-upload",function(e){
+      $("#upload-photo").click()
+   })
+
+
+
+   $("[data-template]").each(function(){
+      let id = $(this).data("template");
+      let template = $(id).html();
+      $(this).html(template);
+   })
+
+});
+
+
---
+++ lu.yuti/js/signin.js
@@ -0,0 +1,30 @@
+const checkSigninForm = () => {
+	let username = $("#signin-username").val();
+	let password = $("#signin-password").val();
+	
+	if(username=="user" && password=="pass") {
+      console.log("logged in")
+      sessionStorage.userId = 3;
+   } else {
+      console.log("logged out")
+      sessionStorage.removeItem("userId");
+   }
+
+    checkUserId();
+}
+
+
+const checkUserId = () => {
+   let p = ["#signin-page","#signup-page",""];
+
+    if(sessionStorage.userId === undefined) {
+      // not logged in
+      if(!p.some(o=>window.location.hash===o))
+         $.mobile.navigate("#signin-page");
+   } else {
+      // logged in
+      if(p.some(o=>window.location.hash===o))
+         $.mobile.navigate("#recent-page");
+   }
+}
+
